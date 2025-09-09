137166d00f0527484d5de74fd09e5262a8219ee0
leey00nsu <dbstndla1212@naver.com>
2025-09-09 14:01:32 +0900

    feat: 폰트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import localFont from 'next/font/local'
 import { Container } from '@/widgets/layout/ui/container'
 import { SonnerToaster } from '@/shared/ui/sonner-toaster'
+import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const pretendard = localFont({
+  src: './PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
 })
 
 export const metadata: Metadata = {
@@ -25,10 +22,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={pretendard.variable}>
+      <body className={`${pretendard.className} antialiased`}>
         <Container>{children}</Container>
         <SonnerToaster />
       </body>

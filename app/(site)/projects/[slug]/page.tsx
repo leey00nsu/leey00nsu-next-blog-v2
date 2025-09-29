7fce6c6ff82d9f99257c1fa7241714b1df77c391
leey00nsu <dbstndla1212@naver.com>
2025-09-29 13:10:11 +0900

    fix: locale prop 추가

--- app/(site)/projects/[slug]/page.tsx
+++ app/(site)/projects/[slug]/page.tsx
@@ -68,5 +68,5 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
     notFound()
   }
 
-  return <ProjectDetail project={project} />
+  return <ProjectDetail project={project} locale={locale} />
 }
